7c3a3cb515103fc748a6b3bac6864e31a1e4d9e3
José Luis Olivares <joseluis_503@yahoo.com>
2025-07-02 17:46:55 -0600

    [Add] Agregando  console log para mostrar puertos de despliegue de la app en el dist

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true })); // This will apply validation globally to all routes
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Application is running on: ${process.env.PORT ?? 3000}`);
+  console.log(`Application is running on: ${process.env.NODE_ENV ?? 'development'}`);
 }
 bootstrap();
